fd39e02407305e9bcb1ac3d26a4eb2ecd3ce550e
Bitodette <farriiedd@gmail.com>
2026-06-18 13:50:12 +0700

    fix: hotbar

--- Assets/Script/HotbarManager.cs
+++ Assets/Script/HotbarManager.cs
@@ -1,9 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[ExecuteInEditMode]
 public class HotbarManager : MonoBehaviour
 {
     [Header("UI References")]
     [SerializeField] private SlotUI[] uiSlots;
+    [SerializeField] private GameObject slotPrefab;
+    [SerializeField] private Sprite selectedSlotSprite;
+
+    [Header("Layout")]
+    [SerializeField] private float slotWidth = 70f;
+    [SerializeField] private float slotHeight = 70f;
+    [SerializeField] private float spacing = 6f;
+    [SerializeField] private float yOffset = 0f;
 
     [Header("Inventory Data")]
     private int hotbarSize = 9;
@@ -24,24 +38,98 @@ public class HotbarManager : MonoBehaviour
 
     void Awake()
     {
-        Debug.Log("[HotbarManager] Awake called. uiSlots == null? " + (uiSlots == null) + " | length: " + (uiSlots?.Length ?? -1));
+        bool needsGenerate = uiSlots == null || uiSlots.Length == 0;
+        if (!needsGenerate && uiSlots[0] == null) needsGenerate = true;
+
+        if (needsGenerate)
+        {
+            GenerateSlots();
+            if (slotPrefab == null) return;
+        }
+
+        Debug.Log("[HotbarManager] Awake called. uiSlots length: " + (uiSlots?.Length ?? -1));
+
+        if (!Application.isPlaying) return;
+
         hotbarSize = uiSlots.Length;
         inventoryItems = new ItemData[hotbarSize];
         itemCounts = new int[hotbarSize];
     }
 
+    void GenerateSlots()
+    {
+        if (slotPrefab == null)
+        {
+            Debug.LogError("[HotbarManager] slotPrefab is not assigned! Cannot generate slots.");
+            return;
+        }
+
+        uiSlots = new SlotUI[9];
+        float totalWidth = 9 * slotWidth + 8 * spacing;
+        float startX = -totalWidth / 2f + slotWidth / 2f;
+
+        for (int i = 0; i < 9; i++)
+        {
+            GameObject slotObj = Instantiate(slotPrefab, transform);
+            slotObj.name = "Slot_" + (i + 1);
+
+            RectTransform rt = slotObj.GetComponent<RectTransform>();
+            rt.anchorMin = new Vector2(0.5f, 0.5f);
+            rt.anchorMax = new Vector2(0.5f, 0.5f);
+            rt.pivot = new Vector2(0.5f, 0.5f);
+            rt.sizeDelta = new Vector2(slotWidth, slotHeight);
+            rt.anchoredPosition = new Vector2(startX + i * (slotWidth + spacing), yOffset);
+            rt.localScale = Vector3.one;
+
+            SlotUI slot = slotObj.GetComponent<SlotUI>();
+            slot.SetSlotIndex(i);
+            slot.SetSelectedSprite(selectedSlotSprite);
+            uiSlots[i] = slot;
+        }
+    }
+
     void Start()
     {
+        if (!Application.isPlaying) return;
         Debug.Log("[HotbarManager] Start called. selectedIndex=" + selectedIndex);
+
+        for (int i = 0; i < uiSlots.Length; i++)
+        {
+            if (uiSlots[i] != null)
+                uiSlots[i].SetSlotIndex(i);
+        }
+
         UpdateHighlightVisual();
         RefreshUI();
     }
 
     void Update()
     {
+        if (!Application.isPlaying) return;
         HandleScrollInput();
         HandleNumberKeys();
     }
+
+#if UNITY_EDITOR
+    [ContextMenu("Regenerate Slots")]
+    void RegenerateInEditor()
+    {
+        if (Application.isPlaying) return;
+
+        for (int i = transform.childCount - 1; i >= 0; i--)
+        {
+            Transform child = transform.GetChild(i);
+            if (child.name.StartsWith("Slot_"))
+                DestroyImmediate(child.gameObject);
+        }
+
+        uiSlots = null;
+        GenerateSlots();
+
+        EditorUtility.SetDirty(this);
+        UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(gameObject.scene);
+    }
+#endif
 
     void HandleScrollInput()
     {
@@ -73,7 +161,7 @@ public class HotbarManager : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.Alpha9)) SetSelected(8);
     }
 
-    void SetSelected(int index)
+    public void SetSelected(int index)
     {
         if (index < 0 || index >= hotbarSize) return;
         selectedIndex = index;
--- Assets/Script/TilemapReadController.cs
+++ Assets/Script/TilemapReadController.cs
@@ -1,27 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class TilemapReadController : MonoBehaviour
 {
     [SerializeField] Tilemap tilemap;
+    [SerializeField] float useRange = 3f;
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            GetTileBase(Input.mousePosition);
+            if (IsPointerOverUI()) return;
+            TryUseTile(Input.mousePosition);
         } 
     }
+
+    private bool IsPointerOverUI()
+    {
+        return UnityEngine.EventSystems.EventSystem.current != null
+            && UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject();
+    }
+
+    private void TryUseTile(Vector2 mousePosition)
+    {
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        worldPosition.z = 0;
+        Vector3Int gridPosition = tilemap.WorldToCell(worldPosition);
+        TileBase tileBase = tilemap.GetTile(gridPosition);
+
+        if (tileBase == null) return;
+
+        float dist = Vector2.Distance(worldPosition, GetPlayerPosition());
+        if (dist > useRange) return;
+
+        Debug.Log("Tile in position = " + gridPosition + " is " + tileBase);
+    }
+
+    private Vector2 GetPlayerPosition()
+    {
+        if (GameManager.Instance != null && GameManager.Instance.player != null)
+            return GameManager.Instance.player.transform.position;
+        return Vector2.zero;
+    }
+
     public TileBase GetTileBase(Vector2 mousePosition)
     {
         Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        
+        worldPosition.z = 0;
         Vector3Int gridPosition = tilemap.WorldToCell(worldPosition);
-        
-        TileBase tileBase = tilemap.GetTile(gridPosition);
-
-        Debug.Log("Tile in position = " + gridPosition + " is " + tileBase);
-        return tileBase;
+        return tilemap.GetTile(gridPosition);
     }
 }
